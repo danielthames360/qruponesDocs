26cb0b207ae0d337ff49bd20f714f48f0cf2b3b8
Daniel Cespedes <danielthames360@gmail.com>
2023-09-29 18:00:56 -0400

    documentation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,17 +9,16 @@ const config = {
   title: 'Qrupones',
   tagline: 'Documentación de integración para el sistema Qrupones',
   favicon: 'img/favicon.ico',
-
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://qruponesdocs-production.up.railway.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'qrupones', // Usually your GitHub org/user name.
+  projectName: 'qruponesDocs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
